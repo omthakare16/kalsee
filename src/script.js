0e0022beae57426413efa770126860ab11ecff8c
Om Thakare <omthakare1606@gmail.com>
2022-11-15 11:13:12 +0530

    done adding single equation feature

--- src/script.js
+++ src/script.js
@@ -1,75 +1,75 @@
-const disp =document.getElementById("disp")
+const disp = document.getElementById("disp")
 const btns = document.querySelectorAll("button")
 const clear = document.getElementById("clr")
 const del = document.getElementById("<=")
-let s="";
+let s = "";
 
 console.log(Array.from(btns));
-Array.from(btns).forEach((button)=>{
-    if(button.id=="<="){
-        button.addEventListener('click',(e)=>{
-    
-            s=s.slice(0,-1)
-            // console.log(s);
-            disp.innerHTML=s
-            
-        })
-    }
-    else if(button.id=="ans"){
-        button.addEventListener('click',(e)=>{
-           for (let index = 0; index < s.length; index++) {
-            if(s[index]=='+'){
-                let k=s.slice(0,index)
-                let l= s.slice(index+1,s.length)
-                k= parseFloat(k);
-                l= parseFloat(l);
-                // console.log(add(k,l));
-                s=add(k,l)
-                disp.innerHTML=s
-            }
-            else if(s[index]=='-'){
-                let k=s.slice(0,index)
-                let l= s.slice(index+1,s.length)
-                k= parseFloat(k);
-                l= parseFloat(l);
-                // console.log(add(k,l));
-                s=subtract(k,l)
-                disp.innerHTML=s
-            }
-            else if(s[index]=='x'){
-                let k=s.slice(0,index)
-                let l= s.slice(index+1,s.length)
-                k= parseFloat(k);
-                l= parseFloat(l);
-                // console.log(add(k,l));
-                s=multiply(k,l)
-                disp.innerHTML= s
-            }
-            else if(s[index]=='/'){
-                let k=s.slice(0,index)
-                let l= s.slice(index+1,s.length)
-                k= parseFloat(k);
-                l= parseFloat(l);
-                // console.log(add(k,l));
-                s=divide(k,l).toFixed(2);
-                disp.innerHTML= s
-            }
-            
-           } 
-        })
-    }
-    else{
-        button.addEventListener('click',(e)=>{
-        // console.log(e.target.innerHTML);
-        s=s+e.target.innerHTML
-        disp.innerHTML=s
+Array.from(btns).forEach((button) => {
+  if (button.id == "<=") {
+    button.addEventListener('click', (e) => {
+
+      s = s.slice(0, -1)
+      // console.log(s);
+      disp.innerHTML = s
+
     })
-    }
-    
+  }
+  else if (button.id == "ans") {
+    button.addEventListener('click', (e) => {
+      for (let index = 1; index < s.length; index++) {
+        if (s[index] == '+') {
+          let k = s.slice(0, index)
+          let l = s.slice(index + 1, s.length)
+          k = parseFloat(k);
+          l = parseFloat(l);
+          // console.log(add(k,l));
+          s = add(k, l)
+          disp.innerHTML = s
+        }
+        else if (s[index] == '-') {
+          let k = s.slice(0, index)
+          let l = s.slice(index + 1, s.length)
+          k = parseFloat(k);
+          l = parseFloat(l);
+          // console.log(add(k,l));
+          s = subtract(k, l)
+          disp.innerHTML = s
+        }
+        else if (s[index] == 'x') {
+          let k = s.slice(0, index)
+          let l = s.slice(index + 1, s.length)
+          k = parseFloat(k);
+          l = parseFloat(l);
+          // console.log(add(k,l));
+          s = multiply(k, l)
+          disp.innerHTML = s
+        }
+        else if (s[index] == '/') {
+          let k = s.slice(0, index)
+          let l = s.slice(index + 1, s.length)
+          k = parseFloat(k);
+          l = parseFloat(l);
+          // console.log(add(k,l));
+          s = divide(k, l).toFixed(2);
+          disp.innerHTML = s
+        }
+
+      }
+    })
+  }
+  else {
+    button.addEventListener('click', (e) => {
+      // console.log(e.target.innerHTML);
+      s = s + e.target.innerHTML
+      disp.innerHTML = s
+    })
+  }
+
 })
-clear.addEventListener('click',(e)=>{
-    disp.innerHTML=0
-    s="";
+clear.addEventListener('click', (e) => {
+  disp.innerHTML = 0
+  s = "";
 })
 
 
@@ -92,8 +92,8 @@ const sum = function(arr) {
   }
   return element;
 };
-const divide = function(a,b){
-    return a/b;
+const divide = function(a, b) {
+  return a / b;
 }
 const multiply = function(a, b) {
   return a * b;
